c49ec7ee0d07b96fe4b869f92eeaf571007bd479
dvaumoron <dvaumoron@gmail.com>
2023-02-03 08:49:16 +0100

    add utility functions

--- client.go
+++ client.go
@@ -18,8 +18,14 @@
 package puzzlemongoclient
 
 import (
+	"context"
+	"log"
 	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
@@ -27,4 +33,30 @@ func Create() (*options.ClientOptions, string) {
 	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_SERVER_ADDR"))
 	databaseName := os.Getenv("MONGODB_SERVER_DB")
 	return clientOptions, databaseName
+}
+
+func Disconnect(client *mongo.Client, ctx context.Context) {
+	if err := client.Disconnect(ctx); err != nil {
+		log.Print("Error during MongoDB disconnect :", err)
+	}
+}
+
+func ExtractCreateDate(doc bson.M) time.Time {
+	id, _ := doc["_id"].(primitive.ObjectID)
+	return id.Timestamp()
+}
+
+func ExtractUint64(value any) uint64 {
+	switch casted := value.(type) {
+	case int32:
+		return uint64(casted)
+	case int64:
+		return uint64(casted)
+	}
+	return 0
+}
+
+func ExtractBinary(value any) []byte {
+	binary, _ := value.(primitive.Binary)
+	return binary.Data
 }
